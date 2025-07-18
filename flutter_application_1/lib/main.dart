d016e081326ae26611cefce2dd1bff1d2947aebd
Ayushi Thummar <ayushithummar14@gmail.com>
2025-07-18 13:46:40 +0530

    main.dart file added.

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    Container(
+      width: 300,
+      height: 300,
+      padding: EdgeInsets.all(30),
+      margin: EdgeInsets.all(50),
+      color: const Color.fromARGB(255, 115, 255, 64),
+      child: Container(
+        width: 100,
+        height: 100,
+        color: Colors.blueAccent,
+        child: Container(
+          width: 50,
+          height: 50,
+          padding: EdgeInsets.all(30),
+          margin: EdgeInsets.all(30),
+          color: const Color.fromARGB(255, 255, 230, 68),
+        ),
+      ),
+    ),
+  );
+}
